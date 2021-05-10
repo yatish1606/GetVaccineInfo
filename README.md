cb2026e7f6b5644f195243484b6484645aa8cf8d
Yatish Kelkar <yatish1606@gmail.com>
2021-05-10 11:05:06 +0530

    Update

---
+++ README.md
@@ -0,0 +1,18 @@
+## Get COVID-19 Vaccine Info
+
+A command line application to quickly get information about available vaccine slots near you. Find out more about GoI's CoWIN API [here](https://apisetu.gov.in/public/api/cowin#/)
+
+### Getting started
+
+#### 1. Clone the repository 
+##### `git clone https://github.com/yatish1606/GetVaccineInfo.git`
+
+#### 2. Get into the directory 
+##### `cd GetVaccineInfo`
+
+#### 3. Install required modules
+##### `npm install`
+The tool uses a few modules to display data in a readable and pretty format
+
+#### 4. Run the script
+##### `node index.js`
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')()
 const axios = require('axios').default
 const chalk = require('chalk')
 
-let districtID, stateID, month, dateToBook, ageAbove45
+let districtID, stateID, month, dateToBook, age
 
 const headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' }
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -10,36 +10,28 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states', { headers: headers })
     .then(response => {
         
-        response.data.states
-            .sort((a,b) => a.state_id > b.state_id ? 1 : -1)
-            .forEach(({state_id, state_name}) => {
-                console.log(state_id + ' : ' + state_name)
-            })
-        
+        console.table(response.data.states.sort((a,b) => a.state_id > b.state_id ? 1 : -1), ['state_id', 'state_name'])          
+        console.log()
         stateID = prompt(chalk.bgWhiteBright.black('Enter the ID of your state : '))
 
         axios.get(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateID}`, { headers : headers })
             .then(response => {
                 
-                response.data.districts
-                    .sort((a,b) => a.district_name > b.district_name ? 1 : -1)
-                    .forEach(({district_id, district_name}) => {
-                        console.log(district_id + ' : ' + district_name)
-                    })
-
-                districtID = prompt('Enter the ID of your district')
+                console.table(response.data.districts.sort((a,b) => a.district_name > b.district_name ? 1 : -1))
+                    
+                districtID = prompt(chalk.bgWhiteBright.black('Enter the ID of your district'))
 
                 month = new Date().getMonth()
-                dateToBook = prompt('Enter the day of ' + months[month+1] + ' on which you want to get vaccine')
+                console.log()
+                dateToBook = prompt(chalk.bgWhiteBright.black('Enter the day of ' + months[month] + ' on which you want to get vaccine'))
 
                 if(month.length === 1) month = '0'.concat(month)
                 if(dateToBook.length === 1) dateToBook = '0'.concat(dateToBook)
 
-                ageAbove45 = parseInt(prompt('Enter your age : ')) >= 45
+                console.log()
+                age = prompt(chalk.bgWhiteBright.black('Enter your age : '))
 
-                
-
-                    let uri = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=${districtID}&date=${dateToBook}-${month}-2021`
+                    let uri = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtID}&date=${dateToBook}-${month+1}-2021`
                     axios.get(uri, {
                         headers: {
                             'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
@@ -48,12 +40,22 @@ axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states', { headers: h
                         }
                     })
                         .then(response => {
+
+                            if(response.status !== 200) {
+                                console.log(chalk.bgRed.black('Error recieving data. CoWIN server might be down or there is some application error. Please try again'))
+                                return
+                            }
                             
-                            const { from, to, fee_type, vaccine_fees, sessions} = response.data.centers[0]
+                            const sessions = response.data.sessions.filter(session => session.min_age_limit < parseInt(age))
                             
-                            sessions.forEach(session => {
-                                const { available_capacity, min_age_limit, vaccine, slots } = session
-                            })
+                            if(!sessions.length) {
+                                console.log(chalk.bgRed.black('No vaccine slots available')) 
+                                return
+                            }
+                            
+                            console.log(chalk.bgGreenBright.black('Data found. Showing top 20 results'))
+                            console.table(sessions.slice(0,20), ['center_id', 'name', 'pincode', 'from', 'to', 'available_capacity', 'fee', 'min_age_limit'])
+                            
                         })
                         .catch(err => console.log(err))
                 
